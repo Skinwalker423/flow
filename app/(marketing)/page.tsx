c8137653a814ca95c00f9a53f6c96a807599580a
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-05 21:54:12 -0800

    header

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import { Medal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 
 const MarketingPage = () => {
   return (
@@ -15,6 +18,15 @@ const MarketingPage = () => {
           work forward.
         </div>
       </div>
+      <div className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto'>
+        Collaborate, manage projects, and reach new
+        productivity peaks. From high rises to the home
+        office, the way your team works is unique -
+        accomplish it all with Flow.
+      </div>
+      <Button className='mt-6' size={"lg"} asChild>
+        <Link href={"/sign-up"}>Get Flow for free</Link>
+      </Button>
     </div>
   );
 };
