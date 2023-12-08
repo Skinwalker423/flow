8f1be15e0313c7e9fcda55f20942436acbb264df
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-07 19:18:09 -0800

    logo

---
+++ components/Logo.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import localFont from "next/dist/compiled/@next/font/dist/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+const Logo = () => {
+  return (
+    <Link href={"/"}>
+      <div className='hover:opacity-75 transition items-center gap-x-2 hidden md:flex'>
+        <Image
+          src={"/logoipsum-277.svg"}
+          alt='logo'
+          width={30}
+          height={30}
+        />
+      </div>
+      <p
+        className={cn(
+          "text-lg text-neutral-700 pb-1",
+          headingFont.className
+        )}
+      >
+        Flow
+      </p>
+    </Link>
+  );
+};
+
+export default Logo;
