665bec0a2027b8a69f0a06317ce54de199caf98e
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-10 19:02:31 -0800

    userbutton

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,15 @@
 import { Plus } from "lucide-react";
+import {
+  OrganizationSwitcher,
+  UserButton,
+} from "@clerk/nextjs";
 
 import Logo from "@/components/Logo";
 import { Button } from "@/components/ui/button";
 
 const NavBar = () => {
   return (
-    <nav className='fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+    <nav className='fixed z-50 top-0 w-full h-14 border-b shadow-sm px-4 bg-white flex items-center justify-between'>
       <div className='flex items-center gap-x-4'>
         <div className='hidden md:flex'>
           <Logo />
@@ -22,6 +26,33 @@ const NavBar = () => {
         >
           <Plus />
         </Button>
+      </div>
+      <div className='flex gap-3'>
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl={"/organization:id"}
+          afterSelectOrganizationUrl={"/organization:id"}
+          afterLeaveOrganizationUrl={"/select-org"}
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
       </div>
     </nav>
   );
