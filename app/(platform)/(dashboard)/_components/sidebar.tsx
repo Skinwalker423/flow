efbb7b39848a9f8a8f13706589778003c7660839
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-14 17:48:49 -0800

    sidebar hooks

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,6 +1,67 @@
 "use client";
 
-const SideBar = () => {
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import {
+  useOrganization,
+  useOrganizationList,
+} from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+
+interface SideBarProps {
+  storageKey?: string;
+}
+
+const SideBar = ({
+  storageKey = "t-sidebar-state",
+}: SideBarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<
+    Record<string, any>
+  >(storageKey, {});
+
+  const {
+    organization: activeOrganization,
+    isLoaded: isLoadedOrg,
+  } = useOrganization();
+  const { userMemberships, isLoaded: isLoadedOrgList } =
+    useOrganizationList({
+      userMemberships: {
+        infinite: true,
+      },
+    });
+
+  const defaultAccordianValue = Object.keys(expanded).map(
+    (key) => {
+      if (expanded[key]) {
+        return key;
+      }
+    }
+  );
+
+  if (
+    !isLoadedOrg ||
+    !isLoadedOrgList ||
+    userMemberships.isLoading
+  ) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
   return <div>SideBar</div>;
 };
 
