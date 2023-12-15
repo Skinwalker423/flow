ced3de156edcf44bf6056bd32e05d6b830366b3e
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-14 17:07:45 -0800

    sidebar frame

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,3 +1,5 @@
+import SideBar from "../_components/sidebar";
+
 const OrganizationLayout = ({
   children,
 }: {
@@ -5,10 +7,12 @@ const OrganizationLayout = ({
 }) => {
   return (
     <main className='w-full h-full pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto'>
-      <div className='w-64 shrink-0 bg-red-400 hidden md:block'>
-        {/* sidebar */}
+      <div className='flex gap-x-7'>
+        <div className='w-64 shrink-0 bg-red-400 hidden md:block'>
+          <SideBar />
+        </div>
+        {children}
       </div>
-      {children}
     </main>
   );
 };
