6f2c4d6388e8c79a52cdebb696b9ed20aec408e6
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-18 19:46:14 -0800

    revalidate

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,19 +1,34 @@
+"use server";
+
 import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+const CreateBoard = z.object({
+  title: z.string(),
+});
 
 export async function createBoard(formData: FormData) {
   "use server";
 
-  const formTitle = formData.get("title") as string;
+  const orgId = formData.get("orgId");
+
+  const { title } = CreateBoard.parse({
+    title: formData.get("title"),
+  });
   console.log(
-    "action triggered. Form data username",
-    formTitle
+    "action triggered. Form data board title",
+    title
   );
+  console.log("orgId", orgId);
 
   const res = await db.board.create({
     data: {
-      title: formTitle,
+      title,
     },
   });
+
+  revalidatePath(`/organization/${orgId}`);
 
   console.log("response after creating a borar", res);
 }
--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -1,20 +1,45 @@
 import { createBoard } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
 const OrganizationPage = async ({
   params,
 }: {
   params: { id: string };
 }) => {
+  const boards = await db.board.findMany({});
+  console.log("boards", boards);
+
   return (
-    <form action={createBoard}>
-      <input
-        className='px-4 py-2 border border-blue-500'
-        type='text'
-        name='title'
-        id='title'
-        placeholder='enter board title'
-      />
-    </form>
+    <div className='flex flex-col gap-2'>
+      <form action={createBoard}>
+        <input
+          className='px-4 py-2 border border-blue-500'
+          type='text'
+          name='title'
+          id='title'
+          placeholder='enter board title'
+        />
+        <input
+          className='hidden'
+          type='text'
+          name='orgId'
+          id='orgId'
+          value={params.id}
+          readOnly
+        />
+        <Button type='submit' variant={"primary"}>
+          Submit
+        </Button>
+      </form>
+      <div className='space-y-2'>
+        {boards.map((board) => {
+          return (
+            <div key={board.id}>Title: {board.title}</div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
