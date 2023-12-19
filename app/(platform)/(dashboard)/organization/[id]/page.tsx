8ad3aae525a09d9090d636b5015a66a0d29daf71
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-18 18:10:41 -0800

    org page form

--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import { db } from "@/lib/db";
 
 const OrganizationPage = async ({
   params,
 }: {
   params: { id: string };
 }) => {
+  async function create(formData: FormData) {
+    "use server";
+
+    const userName = formData.get("title");
+    console.log(
+      "action triggered. Form data username",
+      userName
+    );
+
+      db.board.create({
+        data: {
+          title: 
+        }
+      })
+
+  }
+
   return (
-    <div>
-      <h3>OrganizationPage id: {params.id}</h3>
-    </div>
+    <form action={create}>
+      <input
+        className='px-4 py-2 border border-blue-500'
+        type='text'
+        name='title'
+        id='title'
+        placeholder='enter board title'
+      />
+    </form>
   );
 };
 
